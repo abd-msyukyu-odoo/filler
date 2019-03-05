964a1d19daa11d9a132b030c414a9015adf84801
Damien Abeloos <dabeloos@e-r8-p5.s19.be>
2019-03-05 19:54:57 +0100

    play, premices

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: dabeloos <dabeloos@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 15:44:53 by dabeloos          #+#    #+#             */
-/*   Updated: 2019/03/05 18:52:58 by dabeloos         ###   ########.fr       */
+/*   Updated: 2019/03/05 19:52:22 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,12 +91,14 @@ typedef struct			s_ply
  * map : piece's map
  * mic : minimum coordinate
  * mac : maximum coordinate
+ * s : first position
  */
 typedef struct			s_pc
 {
 	t_map				map;
 	t_crd				mic;
 	t_crd				mac;
+	t_crd				s;
 }						t_pc;
 
 /*
--- main/play.c
+++ main/play.c
@@ -6,22 +6,62 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 18:39:13 by dabeloos          #+#    #+#             */
-/*   Updated: 2019/03/05 18:57:51 by dabeloos         ###   ########.fr       */
+/*   Updated: 2019/03/05 19:54:33 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-void			yfind_owned(t_gm *gm)
+static unsigned char	yis_coord(t_crd *crd)
 {
-	
+	return (crd->x ! -1 && crd->y != -1);
 }
 
-void			yplay(t_gm *gm)
+static unsigned char	yfind_owned(t_gm *gm)
+{
+	int			y;
+	int			x;
+
+	gm->me.s = (t_crd){-1, -1};
+	gm->en.s = (t_crd){-1, -1};
+	y = 0;
+	while (y < gm->map.h)
+	{
+		x = 0;
+		while (x < gm->map.w)
+		{
+			if (!yis_coord(gm->en.s) && gm->map.m[y][x].o == gm->en.o)
+				gm->en.s = (t_crd){x, y};
+			else if (!yis_coord(gm->me.s) && gm->map.m[y][x].o == gm->me.o)
+				gm->me.s = (t_crd){x, y};
+			if (yis_coord(gm->en.s) && yis_coord(gm->me.s))
+				return (1);
+			x++;
+		}
+		y++;
+	}
+	gm->en.o = '\0';
+	if (!yis_coord(gm->me.s))
+		return (0);
+	return (1);
+}
+
+void					play(t_gm *gm)
 {
 	//chercher une case de contour du joueur sur la map
 	//chercher une case de contour de la piece (attention debordement)
 	//tester si la piece peut se mettre
 	//a partir d'une piece d'un joueur, on peut facilement trouver toutes les
 	//autres
+	if (!yfind_owned(gm))
+		return ;
+	while (ynext_pos(&(gm->me)))
+	{
+		while (ynext_pc_pos(&(gm->pc))
+		{
+			if (ycan_put_piece(&(gm->me), &(gm->pc)))
+				yput_piece(&(gm->pc.s));
+		}
+		yreset_pc_pos(&(gm->pc));
+	}
 }
